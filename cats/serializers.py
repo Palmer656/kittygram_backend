52a46619a1a2583116a38cd03537ec1af16ea4ff
unknown <Palmer656@yandex.ru>
2024-05-07 17:57:35 +0300

    07.05.2024 18:00

--- cats/serializers.py
+++ cats/serializers.py
@@ -13,29 +13,36 @@ from .models import Achievement, AchievementCat, Cat
 class Hex2NameColor(serializers.Field):
     def to_representation(self, value):
         return value
+
     def to_internal_value(self, data):
         try:
             data = webcolors.hex_to_name(data)
         except ValueError:
-            raise serializers.ValidationError('Для этого цвета нет имени')
+            raise serializers.ValidationError("Для этого цвета нет имени")
         return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
-    achievement_name = serializers.CharField(source='name')
+    achievement_name = serializers.CharField(source="name")
 
     class Meta:
         model = Achievement
-        fields = ('id', 'achievement_name')
+        fields = ("id", "achievement_name")
 
 
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        # Если полученный объект строка, и эта строка
+        # начинается с 'data:image'...
+        if isinstance(data, str) and data.startswith("data:image"):
+            # ...начинаем декодировать изображение из base64.
+            # Сначала нужно разделить строку на части.
+            format, imgstr = data.split(";base64,")
+            # И извлечь расширение файла.
+            ext = format.split("/")[-1]
+            # Затем декодировать сами данные и поместить результат в файл,
+            # которому дать название по шаблону.
+            data = ContentFile(base64.b64decode(imgstr), name="temp." + ext)
 
         return super().to_internal_value(data)
 
@@ -44,49 +51,56 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(required=False, many=True)
     color = Hex2NameColor()
     age = serializers.SerializerMethodField()
+    # Вот оно — новое поле для изображений.
     image = Base64ImageField(required=False, allow_null=True)
-    
+
     class Meta:
         model = Cat
         fields = (
-            'id', 'name', 'color', 'birth_year', 'achievements', 'owner', 'age',
-            'image'
-            )
-        read_only_fields = ('owner',)
+            "id",
+            "name",
+            "color",
+            "birth_year",
+            "achievements",
+            "owner",
+            "age",
+            "image",
+        )
+        read_only_fields = ("owner",)
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
-    
+
     def create(self, validated_data):
-        if 'achievements' not in self.initial_data:
+        if "achievements" not in self.initial_data:
             cat = Cat.objects.create(**validated_data)
             return cat
         else:
-            achievements = validated_data.pop('achievements')
+            achievements = validated_data.pop("achievements")
             cat = Cat.objects.create(**validated_data)
             for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement
-                    )
+                current_achievement, status = (
+                    Achievement.objects.get_or_create(**achievement)
+                )
                 AchievementCat.objects.create(
                     achievement=current_achievement, cat=cat
-                    )
+                )
             return cat
-    
+
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.color = validated_data.get('color', instance.color)
+        instance.name = validated_data.get("name", instance.name)
+        instance.color = validated_data.get("color", instance.color)
         instance.birth_year = validated_data.get(
-            'birth_year', instance.birth_year
-            )
-        instance.image = validated_data.get('image', instance.image)
-        if 'achievements' in validated_data:
-            achievements_data = validated_data.pop('achievements')
+            "birth_year", instance.birth_year
+        )
+        instance.image = validated_data.get("image", instance.image)
+        if "achievements" in validated_data:
+            achievements_data = validated_data.pop("achievements")
             lst = []
             for achievement in achievements_data:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement
-                    )
+                current_achievement, status = (
+                    Achievement.objects.get_or_create(**achievement)
+                )
                 lst.append(current_achievement)
             instance.achievements.set(lst)
 
